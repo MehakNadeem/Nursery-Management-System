6bbf99fe16601c2d7e0dd8c677f22c85babb7582
Mehak Nadeem <89253038+MehakNadeem@users.noreply.github.com>
2023-01-22 00:24:16 +0500

    Nursery-Management-System

---
+++ db_classes.py
@@ -0,0 +1,38 @@
+class User:
+    """user class"""
+    def __init__(self,name='',email='',password=''):
+        self.name=name
+        self.email=email
+        self.password=password
+
+class Customer(User):
+    """customer class"""
+    def __init__(self,name='',email='',password='',phone = '', address = '',
+                 city = '', province = '', zipCode = '', orderCount = 1):
+        User.__init__(name,email,password)
+        self.phone = phone
+        self.address = address
+        self.city = city
+        self.province = province
+        self.zipCode = zipCode
+        self.orderCount = orderCount
+
+class Product:
+    """product class"""
+    def __init__(self,name = '',price = 0.00,quantity = 1,color = '',category = '',
+                 subcategory = '',description = '',image = ''):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.color = color
+        self.category = category
+        self.subcategory = subcategory
+        self.description = description
+        self.image = image
+
+class Order:
+    """order class"""
+    def __init__(self,date='',status='',amount=0.00):
+        self.date = date
+        self.status = status
+        self.amount = amount
---
+++ model.py
@@ -0,0 +1,37 @@
+import pymysql
+from db_classes import *
+
+class UserModel:
+    def __init__(self):
+        self.__connection = None
+        self.__cursor = None
+
+    def connect(self):
+        try:
+            self.__connection = pymysql.connect(host='localhost', user='root', password='pythOn~2022', database='webproject')
+            self.__cursor = self.__connection.cursor()
+        except Exception as e:
+            print(str(e))
+
+    def getUsers(self):
+        user_list = []
+        try:
+            if self.__connection is None:
+                self.connect()
+            query = "select * from user"
+            self.__cursor.execute(query)
+            data = self.__cursor.fetchall()
+            for item in data:
+                user = User(item[1], item[2], item[3])
+                user_list.append(user)
+        except Exception as e:
+            print(str(e))
+        finally:
+            return user_list
+
+    def insertUser(self,user):
+        try:
+            if self.__connection is None:
+                self.connect()
+            query = "insert into "
+
